c0416c1e91f2517fb2ddf2c920b1c69106d478c4
esilosia331 <patience.sikobe@strathmore.edu>
2025-07-29 21:07:07 -0700

    splint 7 complete

---
+++ php/dbConnect.php
@@ -0,0 +1,15 @@
+<?php
+$hostserver = "127.0.0.1"; //ip address:127.0.0.1 or domain:localhost
+$username = "root";
+$password = "";
+$dbname = "test_db";
+
+// Create connection
+$conn = new mysqli($hostserver, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully to the database {$dbname}.";
--- php/phpbasics.php
+++ php/phpbasics.php
@@ -8,4 +8,19 @@ print date("Y-m-d H:i:s");
 print "<br>";
 print date("l");
 
+$yob = 2002;
+
+print $yob;
+$lname = "Esilosia";
+$age = date("Y") - $yob;
+
+print "<br>";
+print $lname . 'is' . $age . 'years old';
+print "<br>";
+if($age >= 23) {
+    print "You are an adult.";
+}else{
+    print "You are a minor.";
+}
+
 ?>
