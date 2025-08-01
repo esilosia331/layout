928b2709a84348b17d324635cb86f1e2326a622b
esilosia331 <patience.sikobe@strathmore.edu>
2025-07-31 21:13:34 -0700

    Update registerpost.php

--- php/registerpost.php
+++ php/registerpost.php
@@ -3,6 +3,12 @@
 // include 'dbConnect.php';
 require 'dbConnect.php';
 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// Create a new user
+
 $username = "admin";
 $password = password_hash("password123", PASSWORD_DEFAULT);
 
